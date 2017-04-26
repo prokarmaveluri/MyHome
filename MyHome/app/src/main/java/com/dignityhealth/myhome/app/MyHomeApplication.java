50e504f5bbf00444096978529f13263b9458949d
kwelsh <kwelsh@prokarmas-MacBook-Pro.local>
2017-04-26 11:59:45 -0600

    Added custom Application. Tweaked Gradle script to allow more custom control of things like analytics, crash reporting...

---
+++ MyHome/app/src/main/java/com/dignityhealth/myhome/app/MyHomeApplication.java
@@ -0,0 +1,32 @@
+package com.dignityhealth.myhome.app;
+
+import android.app.Application;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import com.dignityhealth.myhome.BuildConfig;
+import com.dignityhealth.myhome.R;
+
+import timber.log.Timber;
+
+/**
+ * Created by kwelsh on 4/26/17.
+ */
+
+public class MyHomeApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //Set up logging
+        if (BuildConfig.REPORT_LOGS) {
+            Timber.plant(new Timber.DebugTree());
+        }
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if (prefs.getBoolean(getString(R.string.pref_key_first_run), true)) {
+            //First Run Stuff Here...
+            prefs.edit().putBoolean(getString(R.string.pref_key_first_run), false).apply();
+        }
+    }
+}
