f7f7254fbb7335cec1e527b6910c6c2760613bdb
cmajji <cmajji@prokarma.com>
2017-04-26 15:53:29 -0600

    Added login and enrollment skeleton

---
+++ MyHome/app/src/main/java/com/dignityhealth/myhome/features/enrollment/EnrollmentActivity.java
@@ -0,0 +1,17 @@
+package com.dignityhealth.myhome.features.enrollment;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.dignityhealth.myhome.R;
+
+public class EnrollmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enrollment);
+
+        
+    }
+}
---
+++ MyHome/app/src/main/java/com/dignityhealth/myhome/features/login/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.dignityhealth.myhome.features.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.dignityhealth.myhome.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
+}
