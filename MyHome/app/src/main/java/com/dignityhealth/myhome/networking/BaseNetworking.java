9af1e8caa4f6a3a257f0a1f29fa7cc31f76a9662
kwelsh <kwelsh@prokarmas-MacBook-Pro.local>
2017-04-26 14:30:48 -0600

    Added base networking class

---
+++ MyHome/app/src/main/java/com/dignityhealth/myhome/networking/BaseNetworking.java
@@ -0,0 +1,10 @@
+package com.dignityhealth.myhome.networking;
+
+/**
+ * Created by kwelsh on 4/26/17.
+ * Base networking class that handles interaction with Retrofit
+ */
+
+public class BaseNetworking {
+
+}
