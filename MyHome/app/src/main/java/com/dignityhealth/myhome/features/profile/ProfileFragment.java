dba018092e1105de06c9ea273ac0e55e4be9ea07
kwelsh <kwelsh@prokarmas-MacBook-Pro.local>
2017-05-01 16:38:12 -0600

    Correctly showing Profile information

--- MyHome/app/src/main/java/com/dignityhealth/myhome/features/profile/ProfileFragment.java
+++ MyHome/app/src/main/java/com/dignityhealth/myhome/features/profile/ProfileFragment.java
@@ -10,16 +10,19 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.dignityhealth.myhome.R;
 import com.dignityhealth.myhome.app.BaseFragment;
 import com.dignityhealth.myhome.networking.NetworkManager;
+import com.dignityhealth.myhome.networking.auth.AuthManager;
 import com.dignityhealth.myhome.utils.Constants;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+import timber.log.Timber;
 
 /**
  * Created by kwelsh on 4/26/17.
@@ -27,7 +30,21 @@ import retrofit2.Response;
 
 public class ProfileFragment extends BaseFragment {
     public static final String PROFILE_TAG = "profile_tag";
+
     View profileView;
+    TextView firstName;
+    TextView lastName;
+    TextView dateOfBirth;
+    TextView address;
+    TextView city;
+    TextView state;
+    TextView zip;
+    TextView phone;
+    TextView email;
+
+    TextView insuranceProvider;
+    TextView memberId;
+    TextView group;
 
     public static ProfileFragment newInstance() {
         return new ProfileFragment();
@@ -48,8 +65,22 @@ public class ProfileFragment extends BaseFragment {
         Toolbar toolbar = (Toolbar) profileView.findViewById(R.id.toolbar);
         ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
 
-        //get the profile information
-        //getProfile("Bearer " + AuthManager.getBearerToken());
+        firstName = (TextView) profileView.findViewById(R.id.first_name);
+        lastName = (TextView) profileView.findViewById(R.id.last_name);
+        dateOfBirth = (TextView) profileView.findViewById(R.id.dob);
+        address = (TextView) profileView.findViewById(R.id.address);
+        city = (TextView) profileView.findViewById(R.id.city);
+        state = (TextView) profileView.findViewById(R.id.state);
+        zip = (TextView) profileView.findViewById(R.id.zip);
+        phone = (TextView) profileView.findViewById(R.id.phone);
+        email = (TextView) profileView.findViewById(R.id.email);
+
+        insuranceProvider = (TextView) profileView.findViewById(R.id.provider);
+        memberId = (TextView) profileView.findViewById(R.id.id);
+        group = (TextView) profileView.findViewById(R.id.group);
+
+        //Update the profile information
+        getProfileInfo("Bearer " + AuthManager.getBearerToken());
 
         return profileView;
     }
@@ -81,21 +112,100 @@ public class ProfileFragment extends BaseFragment {
         return Constants.ActivityTag.PROFILE;
     }
 
-    private void getProfile(String bearer){
+    private void getProfileInfo(String bearer) {
         NetworkManager.getInstance().profile(bearer).enqueue(new Callback<ProfileResponse>() {
             @Override
             public void onResponse(Call<ProfileResponse> call, Response<ProfileResponse> response) {
-                if (response.isSuccessful()){
-                    Toast.makeText(getActivity(), "Successful Response\n" + response, Toast.LENGTH_SHORT).show();
+                if (response.isSuccessful()) {
+                    Timber.d("Successful Response\n" + response);
+                    updateProfile(response.body());
                 } else {
-                    Toast.makeText(getActivity(), "Response, but not successful?\n" + response, Toast.LENGTH_SHORT).show();
+                    Timber.e("Response, but not successful?\n" + response);
                 }
             }
 
             @Override
             public void onFailure(Call<ProfileResponse> call, Throwable t) {
-                Toast.makeText(getActivity(), "something failed! :/\n", Toast.LENGTH_SHORT).show();
+                Timber.e("Something failed! :/");
             }
         });
+    }
+
+    private void updateProfile(ProfileResponse profileResponse) {
+        String na = "Not Available";
+
+        if (profileResponse.firstName != null) {
+            firstName.setText(profileResponse.firstName);
+        } else {
+            firstName.setText(na);
+        }
+
+        if (profileResponse.lastName != null) {
+            lastName.setText(profileResponse.lastName);
+        } else {
+            lastName.setText(na);
+        }
+
+        if (profileResponse.dateOfBirth != null) {
+            dateOfBirth.setText(profileResponse.dateOfBirth);
+        } else {
+            dateOfBirth.setText(na);
+        }
+
+        if (profileResponse.address != null && profileResponse.address.line2 != null && profileResponse.address.line1 != null) {
+            address.setText(profileResponse.address.line1 + "\n" + profileResponse.address.line2);
+        } else if (profileResponse.address != null && profileResponse.address.line1 != null) {
+            address.setText(profileResponse.address.line1);
+        } else {
+            address.setText(na);
+        }
+
+        if (profileResponse.address != null && profileResponse.address.city != null) {
+            city.setText(profileResponse.address.city);
+        } else {
+            city.setText(na);
+        }
+
+        if (profileResponse.address != null && profileResponse.address.stateOrProvince != null) {
+            state.setText(profileResponse.address.stateOrProvince);
+        } else {
+            state.setText(na);
+        }
+
+        if (profileResponse.address != null && profileResponse.address.zipCode != null) {
+            zip.setText(profileResponse.address.zipCode);
+        } else {
+            zip.setText(na);
+        }
+
+        if (profileResponse.phoneNumber != null) {
+            phone.setText(profileResponse.phoneNumber);
+        } else {
+            phone.setText(na);
+        }
+
+        if (profileResponse.email != null) {
+            email.setText(profileResponse.email);
+        } else {
+            email.setText(na);
+        }
+
+        if (profileResponse.insuranceProvider != null && profileResponse.insuranceProvider.providerName != null) {
+            insuranceProvider.setText(profileResponse.insuranceProvider.providerName);
+        } else {
+            insuranceProvider.setText(na);
+        }
+
+        if (profileResponse.insuranceProvider != null && profileResponse.insuranceProvider.memberNumber != null) {
+            memberId.setText(profileResponse.insuranceProvider.memberNumber);
+        } else {
+            memberId.setText(na);
+        }
+
+        if (profileResponse.insuranceProvider != null && profileResponse.insuranceProvider.groupNumber != null) {
+            group.setText(profileResponse.insuranceProvider.groupNumber);
+        } else {
+            group.setText(na);
+        }
     }
 }
--- MyHome/app/src/main/java/com/dignityhealth/myhome/features/profile/ProfileResponse.java
+++ MyHome/app/src/main/java/com/dignityhealth/myhome/features/profile/ProfileResponse.java
@@ -6,15 +6,43 @@ package com.dignityhealth.myhome.features.profile;
 
 public class ProfileResponse {
 
-    private String firstName;
-    private String lastName;
-    private String preferredName;
-    private String email;
+    public String firstName;
+    public String middleInitial;
+    public String lastName;
+    public String preferredName;
+    public String gender;
+    public String dateOfBirth;
+    public Address address;
+    public String phoneNumber;
+    public String phoneNumberType;
+    public String contactName;
+    public String contactPhoneNumber;
+    public String primaryCaregiverName;
+    public boolean isPregnant;
+    public String weeksPregnant;
+    public InsuranceProvider insuranceProvider;
+    public String clientID;
+    public String remoteID;
+    public String email;   //Is this in there...
 
-    public ProfileResponse(String firstName, String lastName, String preferredName, String email) {
+    public ProfileResponse(String firstName, String middleInitial, String lastName, String preferredName, String gender, String dateOfBirth, Address address, String phoneNumber, String phoneNumberType, String contactName, String contactPhoneNumber, String primaryCaregiverName, boolean isPregnant, String weeksPregnant, InsuranceProvider insuranceProvider, String clientID, String remoteID, String email) {
         this.firstName = firstName;
+        this.middleInitial = middleInitial;
         this.lastName = lastName;
         this.preferredName = preferredName;
+        this.gender = gender;
+        this.dateOfBirth = dateOfBirth;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.phoneNumberType = phoneNumberType;
+        this.contactName = contactName;
+        this.contactPhoneNumber = contactPhoneNumber;
+        this.primaryCaregiverName = primaryCaregiverName;
+        this.isPregnant = isPregnant;
+        this.weeksPregnant = weeksPregnant;
+        this.insuranceProvider = insuranceProvider;
+        this.clientID = clientID;
+        this.remoteID = remoteID;
         this.email = email;
     }
 
@@ -26,6 +54,53 @@ public class ProfileResponse {
                 ", preferredName='" + preferredName + '\'' +
                 ", email='" + email + '\'' +
                 '}';
+    }
+
+    public class Address {
+        public String line1;
+        public String line2;
+        public String city;
+        public String stateOrProvince;
+        public String zipCode;
+        public String countryCode;
+
+        public Address(String line1, String line2, String city, String stateOrProvince, String zipCode, String countryCode) {
+            this.line1 = line1;
+            this.line2 = line2;
+            this.city = city;
+            this.stateOrProvince = stateOrProvince;
+            this.zipCode = zipCode;
+            this.countryCode = countryCode;
+        }
+
+        @Override
+        public String toString() {
+            return "Address{" +
+                    "line1='" + line1 + '\'' +
+                    ", line2='" + line2 + '\'' +
+                    ", city='" + city + '\'' +
+                    ", stateOrProvince='" + stateOrProvince + '\'' +
+                    ", zipCode='" + zipCode + '\'' +
+                    ", countryCode='" + countryCode + '\'' +
+                    '}';
+        }
+    }
+
+    public class InsuranceProvider{
+        public String providerName;
+        public String insurancePlan;
+        public String groupNumber;
+        public String memberNumber;
+
+        @Override
+        public String toString() {
+            return "InsuranceProvider{" +
+                    "providerName='" + providerName + '\'' +
+                    ", insurancePlan='" + insurancePlan + '\'' +
+                    ", groupNumber='" + groupNumber + '\'' +
+                    ", memberNumber='" + memberNumber + '\'' +
+                    '}';
+        }
     }
 }
 
