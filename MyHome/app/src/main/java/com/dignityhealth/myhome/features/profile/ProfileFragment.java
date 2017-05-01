2dff4eb75b24e1d608a7d711e37c1c49c099ceca
kwelsh <kwelsh@prokarmas-MacBook-Pro.local>
2017-05-01 11:05:07 -0600

    Added an Edit option for the Profile fragment

--- MyHome/app/src/main/java/com/dignityhealth/myhome/features/profile/ProfileFragment.java
+++ MyHome/app/src/main/java/com/dignityhealth/myhome/features/profile/ProfileFragment.java
@@ -2,7 +2,12 @@ package com.dignityhealth.myhome.features.profile;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
@@ -28,15 +33,48 @@ public class ProfileFragment extends BaseFragment {
         return new ProfileFragment();
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         profileView = inflater.inflate(R.layout.profile, container, false);
 
+        //Toolbar
+        Toolbar toolbar = (Toolbar) profileView.findViewById(R.id.toolbar);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+
         //get the profile information
         //getProfile("bearer token here...");
 
         return profileView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(final Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        menu.clear();
+
+        inflater.inflate(R.menu.profile_menu, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                getActivity().onBackPressed();
+                break;
+
+            case R.id.edit_profile:
+                Toast.makeText(getActivity(), "edit profile stuff...", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
