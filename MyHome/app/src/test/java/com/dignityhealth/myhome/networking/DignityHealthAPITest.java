f28a0816e043cbb3e23c249b17babbc813d7b798
kwelsh <kwelsh@prokarmas-MacBook-Pro.local>
2017-05-08 17:40:37 -0600

    Added more to the unit Test for APIs

--- MyHome/app/src/test/java/com/dignityhealth/myhome/networking/DignityHealthAPITest.java
+++ MyHome/app/src/test/java/com/dignityhealth/myhome/networking/DignityHealthAPITest.java
@@ -1,15 +1,22 @@
 package com.dignityhealth.myhome.networking;
 
+import com.dignityhealth.myhome.features.login.LoginRequest;
+import com.dignityhealth.myhome.features.login.LoginResponse;
 import com.dignityhealth.myhome.features.profile.Profile;
+import com.dignityhealth.myhome.features.profile.signout.CreateSessionRequest;
+import com.dignityhealth.myhome.features.profile.signout.CreateSessionResponse;
 import com.dignityhealth.myhome.networking.auth.AuthManager;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
 
 import retrofit2.Call;
+import retrofit2.Callback;
 import retrofit2.Response;
+import timber.log.Timber;
 
 /**
  * Created by kwelsh on 5/4/17.
@@ -19,10 +26,36 @@ import retrofit2.Response;
 public class DignityHealthAPITest {
     private static final String STATIC_BEARER_TOKEN = "Bearer " + "insert_bearer_token_here...";
     private static final String CURRENT_BEARER_TOKEN = "Bearer " + AuthManager.getBearerToken();
+    private static final LoginRequest LOGIN_REQUEST = new LoginRequest("cmajji@gmail.com", "Password123*", new LoginRequest.Options(true, true));
+
+    @Before
+    public void setup(){
+        NetworkManager.getInstance().initService();
+    }
+
+    @Test
+    public void getLogin_Success() {
+        Call<LoginResponse> call = NetworkManager.getInstance().login(LOGIN_REQUEST);
+        try {
+            Response<LoginResponse> response = call.execute();
+            Assert.assertNotNull(response);
+            Assert.assertTrue(response.isSuccessful());
+            LoginResponse loginResponse = response.body();
+            Assert.assertNotNull(loginResponse);
+
+            //Set up session token
+            AuthManager.setSessionToken(response.body().getSessionToken());
+            getSessionId(response.body().getSessionToken());
+
+            //TODO Get Bearer Token
+            //fetchIdToken(response.body().getSessionToken());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Test
     public void getProfile_Success() {
-        NetworkManager.getInstance().initService();
         Assert.assertTrue(AuthManager.getBearerToken() != null && !AuthManager.getBearerToken().isEmpty()); //Check if bearer token in singleton is assigned or not
         Call<Profile> call = NetworkManager.getInstance().getProfile(CURRENT_BEARER_TOKEN);
         try {
@@ -34,5 +67,24 @@ public class DignityHealthAPITest {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void getSessionId(String sessionToken) {
+        CreateSessionRequest request = new CreateSessionRequest(sessionToken);
+        NetworkManager.getInstance().createSession(request).enqueue(new Callback<CreateSessionResponse>() {
+            @Override
+            public void onResponse(Call<CreateSessionResponse> call, Response<CreateSessionResponse> response) {
+                if (response.isSuccessful()) {
+                    Timber.i("Session Id: " + response.body().getId());
+                    AuthManager.setIdTokenForSignOut(response.body().getId());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<CreateSessionResponse> call, Throwable t) {
+                Timber.i("Session Id: Failed" );
+                AuthManager.setIdTokenForSignOut(null);
+            }
+        });
     }
 }
