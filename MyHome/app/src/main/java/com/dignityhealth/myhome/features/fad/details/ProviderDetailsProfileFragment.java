006f7fbd176b96a829d996dea332272d591151d8
kwelsh <kwelsh@prokarmas-MacBook-Pro.local>
2017-05-18 17:54:46 -0600

    Improved Provider Detail View Pager layouts

--- MyHome/app/src/main/java/com/dignityhealth/myhome/features/fad/details/ProviderDetailsProfileFragment.java
+++ MyHome/app/src/main/java/com/dignityhealth/myhome/features/fad/details/ProviderDetailsProfileFragment.java
@@ -6,13 +6,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.dignityhealth.myhome.R;
 
 public class ProviderDetailsProfileFragment extends Fragment {
     public static final String PROVIDER_DETAILS_PROFILE_TAG = "provider_details_profile_tag";
     public static final String PAGER_TITLE = "Profile";
+
     private View profileView;
+    private TextView acceptingNewPatients;
+    private TextView languages;
+    private TextView gender;
+    private TextView experience;
+    private TextView philosophy;
+    private TextView locations;
+
 
     public ProviderDetailsProfileFragment() {
         // Required empty public constructor
@@ -38,6 +47,14 @@ public class ProviderDetailsProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         profileView = inflater.inflate(R.layout.provider_details_profile, container, false);
+
+        acceptingNewPatients = (TextView) profileView.findViewById(R.id.accepting_new_patients);
+        languages = (TextView) profileView.findViewById(R.id.languages);
+        gender = (TextView) profileView.findViewById(R.id.gender);
+        experience = (TextView) profileView.findViewById(R.id.experience);
+        philosophy = (TextView) profileView.findViewById(R.id.philosophy);
+        locations = (TextView) profileView.findViewById(R.id.locations);
+
         return profileView;
     }
 }
