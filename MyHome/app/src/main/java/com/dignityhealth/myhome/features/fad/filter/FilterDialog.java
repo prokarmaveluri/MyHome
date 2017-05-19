55bf9fcde8951f2621c118628a0a2d86390f3aae
cmajji <cmajji@prokarma.com>
2017-05-19 14:11:50 -0600

    Close soft keyboard in filter

--- MyHome/app/src/main/java/com/dignityhealth/myhome/features/fad/filter/FilterDialog.java
+++ MyHome/app/src/main/java/com/dignityhealth/myhome/features/fad/filter/FilterDialog.java
@@ -205,7 +205,8 @@ public class FilterDialog extends DialogFragment implements SuggestionsAdapter.I
                 || event.getAction() == KeyEvent.ACTION_DOWN
                 && event.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
             binding.locationSugg.setVisibility(View.GONE);
-            return true;
+
+            return false;
         }
         return false;
     }
