7d56bea29a5b044e0162f63ff4542aece50a362a
kwelsh <kwelsh@prokarmas-MacBook-Pro.local>
2017-05-30 13:16:07 -0600

    Improved Calendar for Booking. Still needs work...

--- MyHome/app/src/main/java/com/dignityhealth/myhome/features/fad/details/booking/BookingSelectCalendarFragment.java
+++ MyHome/app/src/main/java/com/dignityhealth/myhome/features/fad/details/booking/BookingSelectCalendarFragment.java
@@ -12,7 +12,12 @@ import android.widget.TextView;
 
 import com.dignityhealth.myhome.R;
 import com.dignityhealth.myhome.features.fad.details.ProviderDetailsResponse;
+import com.dignityhealth.myhome.utils.DateUtil;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by kwelsh on 5/25/17.
@@ -26,6 +31,7 @@ public class BookingSelectCalendarFragment extends Fragment {
     public BookingDateHeaderInterface selectTimeInterface;
 
     View bookingView;
+    TextView monthLabel;
 
     public static BookingSelectCalendarFragment newInstance() {
         return new BookingSelectCalendarFragment();
@@ -45,13 +51,17 @@ public class BookingSelectCalendarFragment extends Fragment {
         Bundle args = getArguments();
         bookingView = inflater.inflate(R.layout.book_calendar, container, false);
 
-        MaterialCalendarView calendar = (MaterialCalendarView) bookingView.findViewById(R.id.calendar);
+        final MaterialCalendarView calendar = (MaterialCalendarView) bookingView.findViewById(R.id.calendar);
         calendar.setTopbarVisible(false);
+        calendar.setPagingEnabled(false);
+        calendar.state().edit().setMinimumDate(Calendar.getInstance()).commit();
+        calendar.setDateSelected(Calendar.getInstance(), true);
 
         RelativeLayout dateHeader = (RelativeLayout) bookingView.findViewById(R.id.date_header);
         ImageView leftArrow = (ImageView) dateHeader.findViewById(R.id.left_date_arrow);
-        ImageView rightArrow = (ImageView) dateHeader.findViewById(R.id.left_date_arrow);
-        TextView monthLabel = (TextView) dateHeader.findViewById(R.id.date);
+        ImageView rightArrow = (ImageView) dateHeader.findViewById(R.id.right_date_arrow);
+        monthLabel = (TextView) dateHeader.findViewById(R.id.date);
+        setMonthHeader(calendar.getSelectedDate());
 
         leftArrow.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,6 +69,14 @@ public class BookingSelectCalendarFragment extends Fragment {
                 if (selectTimeInterface != null) {
                     selectTimeInterface.onBackArrowClicked();
                 }
+
+                Calendar cal = calendar.getCurrentDate().getCalendar();
+                cal.add(Calendar.DATE, -1);
+                CalendarDay calendarDay = CalendarDay.from(cal);
+                calendar.setSelectedDate((Date) null);
+                calendar.setDateSelected(calendarDay, true);
+                calendar.setCurrentDate(calendarDay, true);
+                setMonthHeader(calendarDay);
             }
         });
 
@@ -68,6 +86,14 @@ public class BookingSelectCalendarFragment extends Fragment {
                 if (selectTimeInterface != null) {
                     selectTimeInterface.onFrontArrowClicked();
                 }
+
+                Calendar cal = calendar.getCurrentDate().getCalendar();
+                cal.add(Calendar.DATE, 1);
+                CalendarDay calendarDay = CalendarDay.from(cal);
+                calendar.setSelectedDate((Date) null);
+                calendar.setDateSelected(calendarDay, true);
+                calendar.setCurrentDate(calendarDay, true);
+                setMonthHeader(calendarDay);
             }
         });
 
@@ -81,6 +107,10 @@ public class BookingSelectCalendarFragment extends Fragment {
         });
 
         return bookingView;
+    }
+
+    public void setMonthHeader(CalendarDay calendarDay){
+        monthLabel.setText(DateUtil.convertDateToReadable(calendarDay.getDate()));
     }
 
 //    /**
