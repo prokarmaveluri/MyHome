cc9783ecbc3ca1e2ff0c7d3e04e51c725b947389
kwelsh <kwelsh@prokarmas-MacBook-Pro.local>
2017-05-31 16:46:28 -0600

    Moved Date Constants to Date Util

--- MyHome/app/src/main/java/com/dignityhealth/myhome/utils/DateUtil.java
+++ MyHome/app/src/main/java/com/dignityhealth/myhome/utils/DateUtil.java
@@ -1,8 +1,10 @@
 package com.dignityhealth.myhome.utils;
 
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 import timber.log.Timber;
 
@@ -12,6 +14,27 @@ import timber.log.Timber;
 
 public class DateUtil {
 
+    //Date formats
+    public static final String DATE_FORMAT = "MM/dd/yy";
+    public static final SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat(DATE_FORMAT, Locale.US);
+    public static final String DATE_SHORT_WORDS_FORMAT = "EEE MMM dd";
+    public static final SimpleDateFormat SIMPLE_DATE_SHORT_WORDS_FORMAT = new SimpleDateFormat(DATE_SHORT_WORDS_FORMAT, Locale.US);
+    public static final String DATE_WORDS_1st_FORMAT = "MMMM d'st,' yyyy";   //Adds a "st" to the day (like 1st)
+    public static final SimpleDateFormat SIMPLE_DATE_WORDS_1st_FORMAT = new SimpleDateFormat(DATE_WORDS_1st_FORMAT, Locale.US);
+    public static final String DATE_WORDS_2nd_FORMAT = "MMMM d'nd,' yyyy";   //Adds a "nd" to the day (like 2nd)
+    public static final SimpleDateFormat SIMPLE_DATE_WORDS_2nd_FORMAT = new SimpleDateFormat(DATE_WORDS_2nd_FORMAT, Locale.US);
+    public static final String DATE_WORDS_3rd_FORMAT = "MMMM d'rd,' yyyy";   //Adds a "rd" to the day (like 3rd)
+    public static final SimpleDateFormat SIMPLE_DATE_WORDS_3rd_FORMAT = new SimpleDateFormat(DATE_WORDS_3rd_FORMAT, Locale.US);
+    public static final String DATE_WORDS_4th_FORMAT = "MMMM d'th,' yyyy";   //Adds a "th" to the day (like 4th)
+    public static final SimpleDateFormat SIMPLE_DATE_WORDS_4th_FORMAT = new SimpleDateFormat(DATE_WORDS_4th_FORMAT, Locale.US);
+    public static final String TIME_FORMAT = "h:mm a";
+    public static final SimpleDateFormat SIMPLE_TIME_FORMAT = new SimpleDateFormat(TIME_FORMAT, Locale.US);
+    //public static final String DATE_FORMAT_UTC = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";    //For Profile to work, need to remove milliseconds
+    public static final String DATE_FORMAT_UTC = "yyyy-MM-dd'T'HH:mm:ss'Z'";    //Some appointments will appear ugly without milliseconds...
+    public static final SimpleDateFormat SIMPLE_DATE_FORMAT_UTC = new SimpleDateFormat(DATE_FORMAT_UTC, Locale.US);
+    public static final String DATE_RANGE_FORMAT_UTC = "yyyy-MM-dd'T'HH:mm:ss-HH:mm";    //For appointments with time slots
+    public static final SimpleDateFormat SIMPLE_DATE_RANGE_FORMAT_UTC = new SimpleDateFormat(DATE_RANGE_FORMAT_UTC, Locale.US);
+
     /**
      * Convert a UTC date to a more "human-friendly" format.
      *
@@ -20,8 +43,8 @@ public class DateUtil {
      */
     public static String convertUTCtoReadable(String utcDate) {
         try {
-            Date date = Constants.SIMPLE_DATE_FORMAT_UTC.parse(utcDate);
-            return Constants.SIMPLE_DATE_FORMAT.format(date);
+            Date date = SIMPLE_DATE_FORMAT_UTC.parse(utcDate);
+            return SIMPLE_DATE_FORMAT.format(date);
         } catch (ParseException e) {
             Timber.e("Could not format UTC date " + utcDate + " correctly!\n" + e);
             e.printStackTrace();
@@ -38,8 +61,8 @@ public class DateUtil {
      */
     public static String convertReadableToUTC(String readableDate) {
         try {
-            Date date = Constants.SIMPLE_DATE_FORMAT.parse(readableDate);
-            return Constants.SIMPLE_DATE_FORMAT_UTC.format(date);
+            Date date = SIMPLE_DATE_FORMAT.parse(readableDate);
+            return SIMPLE_DATE_FORMAT_UTC.format(date);
         } catch (ParseException e) {
             Timber.e("Could not format readable date " + readableDate + " correctly!\n" + e);
             e.printStackTrace();
@@ -55,7 +78,7 @@ public class DateUtil {
      * @return a human-friendly string of the given date
      */
     public static String convertDateToReadable(Date date) {
-        return Constants.SIMPLE_DATE_FORMAT.format(date);
+        return SIMPLE_DATE_FORMAT.format(date);
     }
 
     /**
@@ -65,7 +88,7 @@ public class DateUtil {
      * @return a UTC string of the given date
      */
     public static String convertDateToUTC(Date date) {
-        return Constants.SIMPLE_DATE_FORMAT_UTC.format(date);
+        return SIMPLE_DATE_FORMAT_UTC.format(date);
     }
 
     /**
@@ -77,8 +100,8 @@ public class DateUtil {
      */
     public static String getDateWordsFromUTC(String utcDate) {
         try {
-            Date date = Constants.SIMPLE_DATE_FORMAT_UTC.parse(utcDate);
-            return Constants.SIMPLE_DATE_SHORT_WORDS_FORMAT.format(date);
+            Date date = SIMPLE_DATE_FORMAT_UTC.parse(utcDate);
+            return SIMPLE_DATE_SHORT_WORDS_FORMAT.format(date);
         } catch (ParseException e) {
             Timber.e("Could not format UTC date " + utcDate + " correctly!\n" + e);
             e.printStackTrace();
@@ -97,7 +120,7 @@ public class DateUtil {
      */
     public static String getDateWords2FromUTC(String utcDate) {
         try {
-            Date date = Constants.SIMPLE_DATE_FORMAT_UTC.parse(utcDate);
+            Date date = SIMPLE_DATE_FORMAT_UTC.parse(utcDate);
 
             Calendar cal = Calendar.getInstance();
             cal.setTime(date);
@@ -105,13 +128,13 @@ public class DateUtil {
             if (!((day > 10) && (day < 19)))
                 switch (day % 10) {
                     case 1:
-                        return Constants.SIMPLE_DATE_WORDS_1st_FORMAT.format(date);
+                        return SIMPLE_DATE_WORDS_1st_FORMAT.format(date);
                     case 2:
-                        return Constants.SIMPLE_DATE_WORDS_2nd_FORMAT.format(date);
+                        return SIMPLE_DATE_WORDS_2nd_FORMAT.format(date);
                     case 3:
-                        return Constants.SIMPLE_DATE_WORDS_3rd_FORMAT.format(date);
+                        return SIMPLE_DATE_WORDS_3rd_FORMAT.format(date);
                     default:
-                        return Constants.SIMPLE_DATE_WORDS_4th_FORMAT.format(date);
+                        return SIMPLE_DATE_WORDS_4th_FORMAT.format(date);
                 }
         } catch (ParseException e) {
             Timber.e("Could not format UTC date " + utcDate + " correctly!\n" + e);
@@ -123,8 +146,8 @@ public class DateUtil {
 
     public static String getTimeFromUTC(String utcDate) {
         try {
-            Date date = Constants.SIMPLE_DATE_FORMAT_UTC.parse(utcDate);
-            return Constants.SIMPLE_TIME_FORMAT.format(date);
+            Date date = SIMPLE_DATE_FORMAT_UTC.parse(utcDate);
+            return SIMPLE_TIME_FORMAT.format(date);
         } catch (ParseException e) {
             Timber.e("Could not format UTC date " + utcDate + " correctly!\n" + e);
             e.printStackTrace();
@@ -135,9 +158,9 @@ public class DateUtil {
 
     public static String getTimeRangeFromUTC(String utcDate) {
         try {
-            Date date = Constants.SIMPLE_DATE_RANGE_FORMAT_UTC.parse(utcDate);
+            Date date = SIMPLE_DATE_RANGE_FORMAT_UTC.parse(utcDate);
             Timber.i(date.toString());
-            return Constants.SIMPLE_TIME_FORMAT.format(date);
+            return SIMPLE_TIME_FORMAT.format(date);
         } catch (ParseException e) {
             Timber.e("Could not format UTC date " + utcDate + " correctly!\n" + e);
             e.printStackTrace();
@@ -154,7 +177,7 @@ public class DateUtil {
      */
     public static long getMilliseconds(String utcDate) {
         try {
-            Date date = Constants.SIMPLE_DATE_FORMAT_UTC.parse(utcDate);
+            Date date = SIMPLE_DATE_FORMAT_UTC.parse(utcDate);
             return date.getTime();
         } catch (ParseException e) {
             Timber.e("Could not format UTC date " + utcDate + " correctly!\n" + e);
