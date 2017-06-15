56aa4687f83ddef3c9b587b80d000005afbdaf6e
cmajji <cmajji@prokarma.com>
2017-06-15 16:53:35 -0600

    Home changes

--- MyHome/app/src/main/java/com/dignityhealth/myhome/features/home/HomeFragment.java
+++ MyHome/app/src/main/java/com/dignityhealth/myhome/features/home/HomeFragment.java
@@ -1,40 +1,36 @@
 package com.dignityhealth.myhome.features.home;
 
-import android.graphics.Color;
+import android.app.Activity;
+import android.databinding.DataBindingUtil;
 import android.graphics.Typeface;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.content.ContextCompat;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.SpannableStringBuilder;
 import android.text.TextPaint;
 import android.text.method.LinkMovementMethod;
 import android.text.style.ClickableSpan;
-import android.text.style.ForegroundColorSpan;
 import android.text.style.StyleSpan;
 import android.text.style.TextAppearanceSpan;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.ProgressBar;
-import android.widget.RelativeLayout;
+import android.view.inputmethod.EditorInfo;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.dignityhealth.myhome.R;
 import com.dignityhealth.myhome.app.BaseFragment;
 import com.dignityhealth.myhome.app.NavigationActivity;
+import com.dignityhealth.myhome.databinding.HomeBinding;
 import com.dignityhealth.myhome.features.appointments.Appointment;
 import com.dignityhealth.myhome.features.appointments.AppointmentResponse;
+import com.dignityhealth.myhome.features.fad.FadFragment;
 import com.dignityhealth.myhome.features.fad.Provider;
 import com.dignityhealth.myhome.features.fad.ProviderListDialog;
 import com.dignityhealth.myhome.features.fad.recent.RecentlyViewedDataSourceDB;
-import com.dignityhealth.myhome.features.profile.Profile;
 import com.dignityhealth.myhome.features.profile.ProfileManager;
 import com.dignityhealth.myhome.features.profile.ProfileResponse;
 import com.dignityhealth.myhome.networking.NetworkManager;
@@ -57,26 +53,13 @@ import timber.log.Timber;
  * Created by kwelsh on 4/26/17.
  */
 
-public class HomeFragment extends BaseFragment {
+public class HomeFragment extends BaseFragment implements TextView.OnEditorActionListener {
+
     public static final String HOME_TAG = "home_tag";
     protected static final String APPOINTMENT_KEY = "appointment_key";
     private static final int RECENT_PROVIDERS = 200;
-    View homeView;
-    LinearLayout linDbParent;
-    TextView txtDbTitle;
-    TextView txtDbAppointContent;
-    TextView txtDbAppointViewAll;
-    ProgressBar progressBar;
-    RelativeLayout relDbAppointItemLayout;
-    TextView txtDbAppointItemDate;
-    TextView txtDbAppointItemTime;
-    TextView txtDbAppointItemFacility;
-    TextView txtDbAppointItemDoctorName;
-    TextView txtDbDidYouKnowFirst;
-    TextView txtDbDidYouKnowSecond;
-    ImageView imgDbAppointItemPinIcon;
-    Button btnDbScheduleAppoint;
-    View viewAppointDivider;
+    private HomeBinding binding;
+
     Appointment appointment = null;
 
     public static HomeFragment newInstance() {
@@ -86,38 +69,23 @@ public class HomeFragment extends BaseFragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        homeView = inflater.inflate(R.layout.home, container, false);
+        binding = DataBindingUtil.inflate(inflater, R.layout.home, container, false);
         ((NavigationActivity) getActivity()).setActionBarTitle(getString(R.string.dashboard));
 
-        linDbParent = (LinearLayout) homeView.findViewById(R.id.lin_db_parent);
-        txtDbTitle = (TextView) homeView.findViewById(R.id.txt_db_title);
-        txtDbAppointContent = (TextView) homeView.findViewById(R.id.txt_db_appoint_content);
-        txtDbAppointViewAll = (TextView) homeView.findViewById(R.id.txt_db_appoint_viewall);
-        progressBar = (ProgressBar) homeView.findViewById(R.id.pg_db_view_progress);
-
-        relDbAppointItemLayout = (RelativeLayout) homeView.findViewById(R.id.rel_db_appoint_item_layout);
-        txtDbAppointItemDate = (TextView) homeView.findViewById(R.id.txt_db_appoint_item_date);
-        txtDbAppointItemTime = (TextView) homeView.findViewById(R.id.txt_db_appoint_item_time);
-        txtDbAppointItemFacility = (TextView) homeView.findViewById(R.id.txt_db_appoint_item_facility);
-        txtDbAppointItemDoctorName = (TextView) homeView.findViewById(R.id.txt_db_appoint_item_doctor_name);
-        txtDbDidYouKnowFirst = (TextView) homeView.findViewById(R.id.txt_db_didyouknow_first);
-        txtDbDidYouKnowSecond = (TextView) homeView.findViewById(R.id.txt_db_didyouknow_second);
-        imgDbAppointItemPinIcon = (ImageView) homeView.findViewById(R.id.img_db_appoint_item_pin_icon);
-        viewAppointDivider = (View) homeView.findViewById(R.id.view_appoint_divider);
-        btnDbScheduleAppoint = (Button) homeView.findViewById(R.id.btn_db_schedule_appoint);
-
         // Read More contents
-        doSpannableChangesReadMore("The Power of Time Off: Why Vacations Are Essential. ", "Read More", txtDbDidYouKnowFirst);
-        doSpannableChangesReadMore("A Little Bit of Color: Don't Let a Sunburn Get You Down This Summer.", "Read More", txtDbDidYouKnowSecond);
+        doSpannableChangesReadMore("The Power of Time Off: Why Vacations Are Essential. ", "Read More",
+                binding.txtDbDidyouknowFirst);
+        doSpannableChangesReadMore("A Little Bit of Color: Don't Let a Sunburn Get You Down This Summer.", "Read More",
+                binding.txtDbDidyouknowSecond);
 
-        txtDbAppointViewAll.setOnClickListener(new View.OnClickListener() {
+        binding.txtDbAppointViewall.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
                 ((NavigationActivity) getActivity()).goToPage(Constants.ActivityTag.APPOINTMENTS);
             }
         });
-        relDbAppointItemLayout.setOnClickListener(new View.OnClickListener() {
+        binding.relDbAppointItemLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Bundle bundle = new Bundle();
@@ -125,18 +93,19 @@ public class HomeFragment extends BaseFragment {
                 ((NavigationActivity) getActivity()).loadFragment(Constants.ActivityTag.APPOINTMENTS_DETAILS, bundle);
             }
         });
-        imgDbAppointItemPinIcon.setOnClickListener(new View.OnClickListener() {
+        binding.imgDbAppointItemPinIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 CommonUtil.getDirections(getActivity(), appointment.facilityAddress);
             }
         });
-        btnDbScheduleAppoint.setOnClickListener(new View.OnClickListener() {
+        binding.btnDbScheduleAppoint.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startListDialog();
+                startListDialog(getActivity());
             }
         });
+        binding.etxtDbFindcare.setOnEditorActionListener(this);
         if (ConnectionUtil.isConnected(getActivity())) {
             // Get Name from profile
             if (ProfileManager.getProfile() == null) {
@@ -144,17 +113,21 @@ public class HomeFragment extends BaseFragment {
                 getProfileInfo(AuthManager.getInstance().getBearerToken());
             } else {
                 Timber.i("Already have a Profile Singleton. Updating the view...");
-                updateProfileViews(ProfileManager.getProfile());
+                updateAppointViews();
             }
-            // Get Appointment Info
-            getAppointmentInfo(AuthManager.getInstance().getBearerToken());
+
+            if (ProfileManager.getAppointments() == null) {
+                // Get Appointment Info
+                getAppointmentInfo(AuthManager.getInstance().getBearerToken());
+            } else {
+                Timber.i("Already have a Profile Singleton. Updating the view...");
+                updateProfileViews();
+            }
         } else {
             Toast.makeText(getActivity(), R.string.no_network_msg,
                     Toast.LENGTH_LONG).show();
         }
-
-
-        return homeView;
+        return binding.getRoot();
     }
 
     @Override
@@ -169,7 +142,7 @@ public class HomeFragment extends BaseFragment {
         return Constants.ActivityTag.HOME;
     }
 
-    private void startListDialog() {
+    private void startListDialog(Activity activity) {
         ArrayList<String> recentlyViewed = RecentlyViewedDataSourceDB.getInstance().getAllProviderEntry();
         if (recentlyViewed != null && recentlyViewed.size() > 0) {
 
@@ -189,12 +162,11 @@ public class HomeFragment extends BaseFragment {
             bundle.putBoolean("PROVIDER_RECENT", true);
             dialog.setArguments(bundle);
             dialog.setTargetFragment(this, RECENT_PROVIDERS);
-            dialog.show(getChildFragmentManager(), "List Dialog");
+            dialog.show(getFragmentManager(), "List Dialog");
         } else {
-            Toast.makeText(getActivity(), getString(R.string.no_recent_providers),
+            Toast.makeText(activity, getString(R.string.no_recent_providers),
                     Toast.LENGTH_LONG).show();
         }
-
     }
 
     private void getProfileInfo(String bearer) {
@@ -208,7 +180,7 @@ public class HomeFragment extends BaseFragment {
                     if (response.isSuccessful()) {
                         Timber.d("Successful Response\n" + response);
                         ProfileManager.setProfile(response.body().result);
-                        updateProfileViews(response.body().result);
+                        updateProfileViews();
                     } else {
                         Timber.e("Response, but not successful?\n" + response);
                     }
@@ -226,14 +198,12 @@ public class HomeFragment extends BaseFragment {
         });
     }
 
-    private void updateProfileViews(Profile profile) {
-        if (profile.preferredName != null || profile.preferredName != null) {
-            txtDbTitle.setText("Welcome home, " + profile.preferredName + "!");
+    private void updateProfileViews() {
+        if (ProfileManager.getProfile().firstName != null || ProfileManager.getProfile().firstName != null) {
+            binding.txtDbTitle.setText("Welcome home, " + ProfileManager.getProfile().firstName + "!");
         } else {
-            txtDbTitle.setText(getString(R.string.not_available));
+            binding.txtDbTitle.setText(getString(R.string.not_available));
         }
-
-
     }
 
     private void getAppointmentInfo(String bearer) {
@@ -246,16 +216,12 @@ public class HomeFragment extends BaseFragment {
                 if (isAdded()) {
                     hideLoading();
                     if (response.isSuccessful()) {
-                        Timber.d("Successful Response\n" + response);
                         AppointmentResponse result = response.body();
-                        ArrayList<Appointment> appointments = result.result.appointments;
-                        updateAppointViews(appointments);
-
+                        ProfileManager.setAppointments(result.result.appointments);
+                        updateAppointViews();
                     } else {
-
                         Timber.e("Response, but not successful?\n" + response);
                     }
-
                 }
             }
 
@@ -271,66 +237,66 @@ public class HomeFragment extends BaseFragment {
     }
 
     private void showLoading() {
-        progressBar.setVisibility(View.VISIBLE);
+        binding.pgDbViewProgress.setVisibility(View.VISIBLE);
 
     }
 
     private void hideLoading() {
-        progressBar.setVisibility(View.GONE);
+        binding.pgDbViewProgress.setVisibility(View.GONE);
 
     }
 
-    private void updateAppointViews(ArrayList<Appointment> appointments) {
-        try {
-            //Attempt to sort the appointments by startTime
-            Collections.sort(appointments);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    private void updateAppointViews() {
+        ArrayList<Appointment> appointments = ProfileManager.getAppointments();
 
         if (appointments != null && appointments.size() > 0) {
-            relDbAppointItemLayout.setVisibility(View.VISIBLE);
-            viewAppointDivider.setVisibility(View.VISIBLE);
+            try {
+                //Attempt to sort the appointments by startTime
+                Collections.sort(appointments);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            binding.relDbAppointItemLayout.setVisibility(View.VISIBLE);
+            binding.viewAppointDivider.setVisibility(View.VISIBLE);
             SpannableStringBuilder builder1 = new SpannableStringBuilder();
             SpannableString partOne = new SpannableString("It looks like you have ");
             SpannableString partTwo = new SpannableString(String.valueOf(appointments.size()));
-            partTwo.setSpan(new StyleSpan(Typeface.BOLD), 0, String.valueOf(appointments.size()).length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            partTwo.setSpan(new StyleSpan(Typeface.BOLD), 0, String.valueOf(appointments.size()).length(),
+                    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
             SpannableString partThree = new SpannableString(" appointment scheduled");
             SpannableString partFour = new SpannableString(" appointments scheduled");
             builder1.append(partOne);
             builder1.append(partTwo);
 
-
             if (appointments.size() == 1) {
                 builder1.append(partThree);
-                txtDbAppointViewAll.setVisibility(View.GONE);
-                txtDbAppointContent.setText(builder1);
+                binding.txtDbAppointViewall.setVisibility(View.GONE);
+                binding.txtDbAppointContent.setText(builder1);
             } else {
                 builder1.append(partFour);
-                txtDbAppointViewAll.setVisibility(View.VISIBLE);
-                txtDbAppointContent.setText(builder1);
+                binding.txtDbAppointViewall.setVisibility(View.VISIBLE);
+                binding.txtDbAppointContent.setText(builder1);
             }
             appointment = appointments.get(0);
             if (appointment.doctorName != null && !appointment.doctorName.isEmpty()) {
-                txtDbAppointItemDoctorName.setText(appointment.doctorName);
+                binding.txtDbAppointItemDoctorName.setText(appointment.doctorName);
             }
 
             if (appointment.facilityName != null && !appointment.facilityName.isEmpty()) {
-                txtDbAppointItemFacility.setText(appointment.facilityName);
+                binding.txtDbAppointItemFacility.setText(appointment.facilityName);
             }
 
             if (appointment.appointmentStart != null && !appointment.appointmentStart.isEmpty()) {
-                txtDbAppointItemDate.setText(DateUtil.getDateWordsFromUTC(appointment.appointmentStart));
-                txtDbAppointItemTime.setText(DateUtil.getTime(appointment.appointmentStart));
+                binding.txtDbAppointItemDate.setText(DateUtil.getDateWordsFromUTC(appointment.appointmentStart));
+                binding.txtDbAppointItemTime.setText(DateUtil.getTime(appointment.appointmentStart));
             }
 
 
         } else {
-            relDbAppointItemLayout.setVisibility(View.GONE);
-            viewAppointDivider.setVisibility(View.GONE);
-            txtDbAppointViewAll.setVisibility(View.GONE);
-            txtDbAppointContent.setText("No appointments scheduled!");
+            binding.relDbAppointItemLayout.setVisibility(View.GONE);
+            binding.viewAppointDivider.setVisibility(View.GONE);
+            binding.txtDbAppointViewall.setVisibility(View.GONE);
+            binding.txtDbAppointContent.setText("No appointments scheduled!");
         }
     }
 
@@ -338,7 +304,8 @@ public class HomeFragment extends BaseFragment {
         SpannableStringBuilder builder1 = new SpannableStringBuilder();
         SpannableString partOne = new SpannableString(firstString);
         SpannableString partTwo = new SpannableString(secondString);
-        partTwo.setSpan(new TextAppearanceSpan(getContext(), R.style.tradeGothicLTStdBd2_20_deepAqua), 0, partTwo.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        partTwo.setSpan(new TextAppearanceSpan(getContext(), R.style.tradeGothicLTStdBd2_20_deepAqua),
+                0, partTwo.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
 
         ClickableSpan span1 = new ClickableSpan() {
             @Override
@@ -358,5 +325,21 @@ public class HomeFragment extends BaseFragment {
         builder1.append(partTwo);
         txtReadmore.setText(builder1);
         txtReadmore.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+
+    @Override
+    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+        if (actionId == EditorInfo.IME_ACTION_DONE ||
+                actionId == EditorInfo.IME_ACTION_SEARCH) {
+
+            if (v.getText().toString().length() > 0) {
+                FadFragment.currentSearchQuery = v.getText().toString();
+                ((NavigationActivity) getActivity()).goToPage(Constants.ActivityTag.FAD);
+            } else {
+                Toast.makeText(getActivity(), getString(R.string.query_empty), Toast.LENGTH_LONG).show();
+            }
+            return true;
+        }
+        return false;
     }
 }
