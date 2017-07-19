d2e32d1296ad1128fbef10a8a5a74d1a1c08ca0b
cmajji <cmajji@prokarma.com>
2017-07-19 09:58:23 -0600

    Fixed 11944: Android - Contact Us email address populated  in email is incorrect and null phone number when not on profile

--- MyHome/app/src/main/java/com/prokarma/myhome/features/contact/ContactUsFragment.java
+++ MyHome/app/src/main/java/com/prokarma/myhome/features/contact/ContactUsFragment.java
@@ -65,7 +65,7 @@ public class ContactUsFragment extends BaseFragment {
     private void composeEmail() {
         Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
         emailIntent.setType("plain/text");
-        emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{"hello@dignityhealth.com"});
+        emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{"hello@dignityhealth.org"});
         emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Ask A Question");
 
         if (ProfileManager.getProfile() != null) {
