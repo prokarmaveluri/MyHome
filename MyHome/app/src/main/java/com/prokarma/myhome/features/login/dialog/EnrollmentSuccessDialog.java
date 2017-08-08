5640724791bb0ba0e4e1e74439b1ab5027c79b6a
cmajji <cmajji@prokarma.com>
2017-08-08 11:16:33 -0600

    Fixed 12384: Android v30 Enrollment confirmation screen has the work email truncated.

--- MyHome/app/src/main/java/com/prokarma/myhome/features/login/dialog/EnrollmentSuccessDialog.java
+++ MyHome/app/src/main/java/com/prokarma/myhome/features/login/dialog/EnrollmentSuccessDialog.java
@@ -2,6 +2,8 @@ package com.prokarma.myhome.features.login.dialog;
 
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.graphics.Typeface;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
@@ -9,6 +11,8 @@ import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.prokarma.myhome.R;
 
@@ -48,8 +52,25 @@ public class EnrollmentSuccessDialog extends DialogFragment {
         super.onCreateDialog(savedInstanceState);
 
         // Use the Builder class for convenient dialog construction
+        TextView textView = new TextView(getActivity());
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setTitle(R.string.enrollment_success_dialog_tittle)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            textView.setTextAppearance(R.style.tradeGothicLTStd_Dynamic18);
+        } else {
+            textView.setTextAppearance(getActivity(), R.style.tradeGothicLTStd_Dynamic18);
+        }
+        LinearLayout layout = new LinearLayout(getActivity());
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.MATCH_PARENT);
+        float density = getResources().getDisplayMetrics().density;
+        int margin = (int) (density * 16);
+        params.setMargins(margin, margin, margin, 0);
+        textView.setTypeface(textView.getTypeface(), Typeface.BOLD);
+        textView.setLayoutParams(params);
+        textView.setText(R.string.enrollment_success_dialog_tittle);
+        layout.addView(textView);
+        builder.setCustomTitle(layout)
+
                 .setMessage(R.string.enrollment_success_dialog_msg)
                 .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
