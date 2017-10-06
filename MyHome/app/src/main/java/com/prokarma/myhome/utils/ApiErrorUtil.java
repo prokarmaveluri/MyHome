1093bf9463549c9fbe9a6e9e04e472c9ee3711de
kwelsh <kwelsh@prokarma.com>
2017-10-06 17:04:33 -0600

    Created methods for get Providers Error handling

--- MyHome/app/src/main/java/com/prokarma/myhome/utils/ApiErrorUtil.java
+++ MyHome/app/src/main/java/com/prokarma/myhome/utils/ApiErrorUtil.java
@@ -41,7 +41,7 @@ public class ApiErrorUtil {
     }
 
     private void invalidPassword(final Context context, final View view) {
-        snackbar = Snackbar.make(view, context.getString(R.string.api_error_incorrect_password), Snackbar.LENGTH_LONG);
+        snackbar = Snackbar.make(view, "This is a really long line of text. Obnoxiously long to test out the character limit for a snackbar. I'm curious how the word wrapping will handle itself on an Android phone. Love, Kevin.", Snackbar.LENGTH_LONG);
         snackbar.show();
     }
 
@@ -181,6 +181,14 @@ public class ApiErrorUtil {
     }
 
     public void findEmailFailed(final Context context, final View view, final Throwable throwable) {
+        genericError(context, view, true);
+    }
+
+    public <T> void getProvidersError(final Context context, final View view, final Response<T> response) {
+        genericError(context, view, true);
+    }
+
+    public void getProvidersFailed(final Context context, final View view, final Throwable throwable) {
         genericError(context, view, true);
     }
 
