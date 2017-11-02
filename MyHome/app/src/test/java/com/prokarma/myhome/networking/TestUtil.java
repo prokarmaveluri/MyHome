7f97f0cf072370358ba882bb67388a6bfb72b0ae
kwelsh <kwelsh@prokarma.com>
2017-11-02 12:36:05 -0700

    Mocked Shared Preferences using Mockito

--- MyHome/app/src/test/java/com/prokarma/myhome/networking/TestUtil.java
+++ MyHome/app/src/test/java/com/prokarma/myhome/networking/TestUtil.java
@@ -1,10 +1,16 @@
 package com.prokarma.myhome.networking;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 import com.prokarma.myhome.features.login.endpoint.SignInRequest;
 import com.prokarma.myhome.features.login.endpoint.SignInResponse;
+import com.prokarma.myhome.networking.auth.AuthManager;
+import com.prokarma.myhome.utils.AppPreferences;
 import com.prokarma.myhome.utils.EnviHandler;
 
 import org.junit.Assert;
+import org.mockito.Mockito;
 
 import java.io.IOException;
 import java.util.Random;
@@ -21,16 +27,26 @@ public class TestUtil {
     public static void setDevEnvironment() {
         EnviHandler.initEnv(EnviHandler.EnvType.DEV);
         NetworkManager.getInstance().initService();
+        setSharedPreferences();
     }
 
     public static void setStagingEnvironment() {
         EnviHandler.initEnv(EnviHandler.EnvType.STAGE);
         NetworkManager.getInstance().initService();
+        setSharedPreferences();
     }
 
     public static void setProdEnvironment() {
         EnviHandler.initEnv(EnviHandler.EnvType.PROD);
         NetworkManager.getInstance().initService();
+        setSharedPreferences();
+    }
+
+    private static void setSharedPreferences(){
+        final Context context = Mockito.mock(Context.class);
+        final SharedPreferences sharedPrefs = Mockito.mock(SharedPreferences.class);
+        Mockito.when(context.getSharedPreferences(AppPreferences.APP_PREFERENCES, Context.MODE_PRIVATE)).thenReturn(sharedPrefs);
+        AuthManager.getInstance().setContext(context);
     }
 
     public static String getLogin(SignInRequest loginRequest) {
