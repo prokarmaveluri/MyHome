d444280ce97effdb3e469ded8fc561fd07ce43a2
svenugopal <svenugopal@prokarma.com>
2017-11-03 11:19:57 +0530

    Bug Id : 2871932 Talk back: Registration: ToS link opens in new app without warning the user is fixed. Have to review the warning message text.

--- MyHome/app/src/main/java/com/prokarma/myhome/features/tos/TosActivity.java
+++ MyHome/app/src/main/java/com/prokarma/myhome/features/tos/TosActivity.java
@@ -1,15 +1,18 @@
 package com.prokarma.myhome.features.tos;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.prokarma.myhome.R;
 import com.prokarma.myhome.app.BaseActivity;
 import com.prokarma.myhome.app.SplashActivity;
@@ -19,7 +22,6 @@ import com.prokarma.myhome.utils.ApiErrorUtil;
 import com.prokarma.myhome.utils.ConnectionUtil;
 import com.prokarma.myhome.utils.Constants;
 import com.prokarma.myhome.utils.TealiumUtil;
-
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -68,8 +70,35 @@ public class TosActivity extends BaseActivity {
             }
         });
 
-        WebView tos = (WebView) findViewById(R.id.terms_of_service);
+        final WebView tos = (WebView) findViewById(R.id.terms_of_service);
         tos.loadUrl(FILE_ANDROID_ASSET_TOS_HTML);
+        tos.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, final String url) {
+
+                if (url.startsWith(String.valueOf(getString(R.string.url)))) {
+                    AlertDialog.Builder alertDialog = new AlertDialog.Builder(TosActivity.this);
+                    alertDialog.setMessage(getString(R.string.browser_alert));
+                    alertDialog.setPositiveButton(getString(R.string.yes), new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            loadURL(url);
+                        }
+                    }).setNeutralButton(getString(R.string.no), new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            tos.stopLoading();
+                        }
+                    }).show();
+                    return true;
+                }
+                loadURL(url);
+                return true;
+            }
+
+            private void loadURL(String url) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+            }
+        });
 
         enrollmentRequest = getIntent().getParcelableExtra(Constants.ENROLLMENT_REQUEST);
     }
