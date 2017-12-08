074c1d0cd36b05d881892d6fe4a9910d680089a3
kwelsh <kwelsh@prokarma.com>
2017-12-08 14:57:03 -0500

    Dependents have their own fragment now

--- MyHome/app/src/main/java/com/prokarma/myhome/features/mycare/MyCareNowFragment.java
+++ MyHome/app/src/main/java/com/prokarma/myhome/features/mycare/MyCareNowFragment.java
@@ -182,7 +182,11 @@ public class MyCareNowFragment extends BaseFragment implements View.OnClickListe
 
         switch (Id) {
             case R.id.personal_info_edit:
-                ((NavigationActivity) getActivity()).loadFragment(Constants.ActivityTag.MY_CARE_PROFILE, null);
+                if(!AwsManager.getInstance().isPatientMainConsumer()){
+                    ((NavigationActivity) getActivity()).loadFragment(Constants.ActivityTag.MY_CARE_PROFILE_DEPENDENT, null);
+                } else {
+                    ((NavigationActivity) getActivity()).loadFragment(Constants.ActivityTag.MY_CARE_PROFILE, null);
+                }
                 break;
             case R.id.medical_history_edit:
                 ((NavigationActivity) getActivity()).loadFragment(Constants.ActivityTag.MY_MED_HISTORY, null);
