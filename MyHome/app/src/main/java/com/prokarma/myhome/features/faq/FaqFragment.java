14874009576c61aeff4bcc035ac11b8ca9611a10
VijayaEluri <veluri@prokarma.com>
2018-01-04 15:51:56 -0700

    31249: Android v1.5.20 - Few fields are not actionable after logging in to Bill Pay

--- MyHome/app/src/main/java/com/prokarma/myhome/features/faq/FaqFragment.java
+++ MyHome/app/src/main/java/com/prokarma/myhome/features/faq/FaqFragment.java
@@ -1,5 +1,8 @@
 package com.prokarma.myhome.features.faq;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -14,9 +17,12 @@ import android.widget.TextView;
 
 import com.prokarma.myhome.R;
 import com.prokarma.myhome.app.BaseFragment;
+import com.prokarma.myhome.utils.CommonUtil;
 import com.prokarma.myhome.utils.ConnectionUtil;
 import com.prokarma.myhome.utils.Constants;
 import com.prokarma.myhome.utils.TealiumUtil;
+
+import timber.log.Timber;
 
 /**
  * Created by kwelsh on 7/28/17.
@@ -54,14 +60,52 @@ public class FaqFragment extends BaseFragment {
         webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
 
         webView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                Timber.d("billpay. override url = " + url);
+
+                if (url.toLowerCase().endsWith(".pdf")) {
+                    loadPdf(view, url);
+                    return true;
+                } else if (url.startsWith("tel:")) {
+                    Intent intent = new Intent(Intent.ACTION_DIAL);
+                    intent.setData(Uri.parse(url));
+                    startActivity(intent);
+                    return true;
+
+                } else if (url.toLowerCase().startsWith("http:") || url.toLowerCase().startsWith("https:")) {
+                    view.loadUrl(url);
+                    return true;
+                }
+                return false;
+            }
+
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-                webView.loadUrl(request.getUrl().toString());
-                return super.shouldOverrideUrlLoading(view, request);
+                Timber.d("billpay. override request url = " + request.getUrl().toString());
+
+                if (request.getUrl().toString().toLowerCase().endsWith(".pdf")) {
+
+                    loadPdf(view, request.getUrl().toString());
+                    return true;
+
+                } else if (request.getUrl().toString().startsWith("tel:")) {
+                    Intent intent = new Intent(Intent.ACTION_DIAL);
+                    intent.setData(Uri.parse(request.getUrl().toString()));
+                    startActivity(intent);
+                    return true;
+
+                } else if (request.getUrl().toString().toLowerCase().startsWith("http:") || request.getUrl().toString().toLowerCase().startsWith("https:")) {
+                    view.loadUrl(request.getUrl().toString());
+                    return true;
+                }
+                return false;
             }
 
             @Override
             public void onPageFinished(WebView view, String url) {
+                Timber.d("billpay. onPageFinished = " + url);
                 super.onPageFinished(view, url);
                 webProgress.setVisibility(View.GONE);
             }
@@ -70,6 +114,15 @@ public class FaqFragment extends BaseFragment {
             public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
                 super.onReceivedError(view, request, error);
                 webProgress.setVisibility(View.GONE);
+                Timber.d("billpay. onReceivedError = " + error);
+            }
+
+            @Override
+            public void onReceivedError(final WebView view, int errorCode, String description,
+                                        final String failingUrl) {
+                super.onReceivedError(view, errorCode, description, failingUrl);
+                webProgress.setVisibility(View.GONE);
+                Timber.d("billpay. onReceivedError = " + description);
             }
         });
 
@@ -81,6 +134,28 @@ public class FaqFragment extends BaseFragment {
             webView.setVisibility(View.GONE);
         }
         return faqView;
+    }
+
+    private void loadPdf(View view, String url) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setDataAndType(Uri.parse(url), "application/pdf");
+
+        boolean foundAppToLoadPdf = true;
+        try {
+            if (intent.resolveActivity(getContext().getPackageManager()) != null) {
+                view.getContext().startActivity(intent);
+            } else {
+                foundAppToLoadPdf = false;
+            }
+
+        } catch (ActivityNotFoundException e) {
+            //user does not have a pdf viewer installed
+            foundAppToLoadPdf = false;
+        }
+
+        if (!foundAppToLoadPdf) {
+            CommonUtil.showToast(getContext(), "No Application found to view PDF files.");
+        }
     }
 
     @Override
