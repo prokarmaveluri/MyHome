5061de6534195109b8c725ca007a68b83bd1a9cc
kwelsh <kwelsh@prokarma.com>
2018-01-08 13:12:54 -0500

    Make sure release APKs don't show "Details" on the Snackbar (those are only for debugging and internal use)

--- MyHome/app/src/main/java/com/prokarma/myhome/utils/ApiErrorUtil.java
+++ MyHome/app/src/main/java/com/prokarma/myhome/utils/ApiErrorUtil.java
@@ -47,7 +47,7 @@ public class ApiErrorUtil {
             });
         }
 
-        if (!AppPreferences.getInstance().getBooleanPreference(Constants.API_HIDE_API_ERROR_INFO) || BuildConfig.FLAVOR.equals("developer")) {
+        if (!BuildConfig.FLAVOR.equals("release") && (!AppPreferences.getInstance().getBooleanPreference(Constants.API_HIDE_API_ERROR_INFO) || BuildConfig.FLAVOR.equals("developer"))) {
 
             // To avoid this error: android.util.AndroidRuntimeException: Calling startActivity() from outside of an Activity  context requires the FLAG_ACTIVITY_NEW_TASK flag.
             // adding FLAG_ACTIVITY_NEW_TASK
